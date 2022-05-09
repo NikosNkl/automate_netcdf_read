6af26377d6890d04e0a2afe54e96bd8325879329
NikosNkl <75068526+NikosNkl@users.noreply.github.com>
2022-05-10 00:32:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,19 +3,32 @@ import numpy as np
 import pandas as pd
 import os
 import glob
-i = 0
+k = 0
+
 for filename in glob.glob('*.nc'): #access all netCDF files in directory
    with open(os.path.join(os.getcwd(), filename), 'r') as f:
 
+    x = []
+    y = []
+    z = []
+
     ds = nc.Dataset(filename) #read file
 
-    print(ds)
+    # print(ds)
 
     saa = ds['solar_azimuth_angle'][:] #read specific value into numpy array
+    print(saa[325][292])
+    for j in range(500):
+     for i in range(500):
+      y.append(j)
+      x.append(i)
+      z.append(saa[i][j])
 
-    print(saa)
+    saa = np.column_stack((y, x, z))
 
-    df = pd.DataFrame(saa) #create pandas dataframe from numpy array
-    df.to_csv('MVIRI{}.txt'.format(i),sep=" ",index=False) #create .txt file with different filename for each iteration
+    # print(saa)
 
-    i+=1
+    df = pd.DataFrame(saa, columns = ['Column','Row','Solar Azimuth Angle']) #create pandas dataframe from numpy array
+    df.to_csv('MVIRI{}.txt'.format(k),sep=" ",index=False) #create .txt file with different filename for each iteration
+
+    k+=1
