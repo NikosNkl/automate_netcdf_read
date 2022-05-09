f0a8e40abd9940c699745b3c6460a9e2605378de
NikosNkl <75068526+NikosNkl@users.noreply.github.com>
2022-05-09 23:17:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import pandas as pd
 import os
 import glob
 i = 0
-for filename in glob.glob('*.nc'):
+for filename in glob.glob('*.nc'): #access all netCDF files in directory
    with open(os.path.join(os.getcwd(), filename), 'r') as f:
 
-    ds = nc.Dataset(filename)
+    ds = nc.Dataset(filename) #read file
 
     print(ds)
 
-    saa = ds['solar_azimuth_angle'][:]
+    saa = ds['solar_azimuth_angle'][:] #read specific value into numpy array
 
     print(saa)
 
-    df = pd.DataFrame(saa)
-    df.to_csv('MVIRI{}.txt'.format(i),sep=" ",index=False)
+    df = pd.DataFrame(saa) #create pandas dataframe from numpy array
+    df.to_csv('MVIRI{}.txt'.format(i),sep=" ",index=False) #create .txt file with different filename for each iteration
 
     i+=1
